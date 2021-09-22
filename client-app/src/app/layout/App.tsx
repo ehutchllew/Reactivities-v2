e1d4e336b8229d854f07ffc4f9f793eb06b6bdd1
Evan <ehutch7@gmail.com>
2021-09-22 10:24:58 -0400

    Adding models folder and IActivity interface.

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Header, List } from "semantic-ui-react";
+import { IActivity } from "../models/activity";
 
 function App() {
-    const [activities, setActivities] = useState<object[]>([]);
+    const [activities, setActivities] = useState<IActivity[]>([]);
     useEffect(() => {
         fetch("https://localhost:5001/api/activities")
             .then((resp) => resp.json())
